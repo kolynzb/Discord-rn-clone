0c440862d4fd818eb48a77d314e88205e707ae65
kolynzb <collinsbenda360@gmail.com>
2022-04-19 05:19:40 +0300

    Project Setup

--- App.tsx
+++ App.tsx
@@ -1,13 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
+
+import "react-native-gesture-handler";
+import { StreamChat } from "stream-chat";
+import { useEffect } from "react";
+
+const API_KEY = "tn5n438ug9v3";
+const client = StreamChat.getInstance(API_KEY);
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  const connectUser = async () => {
+    //sign in with backend and get user token
+
+    await client.connectUser(
+      {
+        id: "vadim",
+        name: "Vadim",
+        image:
+          "https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg",
+      },
+      client.devToken("vadim")
+    );
+  };
+  useEffect(() => {
+    connectUser();
+  }, []);
 
   if (!isLoadingComplete) {
     return null;
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Discord-rn-clone
+
+`expo init Discord -t tabs`
+
+- For chat features used [stream](https://getstream.io/chat/). Get its [documentation](https://getstream.io/chat/docs/sdk/reactnative/) for react-native
